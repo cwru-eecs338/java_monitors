925b569beb9deaca1c4d84717e913d5e3c7c73c6
Gary Doran, Jr <garydoranjr@gmail.com>
2011-02-21 19:10:58 -0500

    Add optional program arguments

--- DiningPhilosophers.java
+++ DiningPhilosophers.java
@@ -4,16 +4,29 @@ public class DiningPhilosophers {
     public static final int PHILOSOPHERS = 20;
     public static final long DURATION = 5000/*ms*/;
 
+    /*
+     * Two optional arguments:
+     * 1st argument: number of philosophers
+     * 2nd argument: duration in seconds
+     */
     public static void main(String[] args)
     throws InterruptedException {
 
+        // Parse arguments
+        int philosophers = PHILOSOPHERS;
+        long duration = DURATION;
+        try {
+            philosophers = Integer.parseInt(args[0]);
+            duration = 1000*Integer.parseInt(args[1]);
+        } catch (Exception e) { }
+
         // Set the table
-        PhilosopherTable table = new PhilosopherTable(PHILOSOPHERS);
+        PhilosopherTable table = new PhilosopherTable(philosophers);
 
         // Create/Start a thread
         // for each philosopher
-        PhilosopherThread[] threads = new PhilosopherThread[PHILOSOPHERS];
-        for(int i = 0; i < PHILOSOPHERS; i++) {
+        PhilosopherThread[] threads = new PhilosopherThread[philosophers];
+        for(int i = 0; i < philosophers; i++) {
             threads[i] = new PhilosopherThread(table, i);
             threads[i].start();
         }
@@ -21,13 +34,13 @@ public class DiningPhilosophers {
         // Wait for them to have profound
         // epistemological insights, etc.
         try {
-            Thread.sleep(DURATION);
+            Thread.sleep(duration);
         } catch(Exception e) {
             e.printStackTrace();
         }
 
         // Time's up...
-        for(int i = 0; i < PHILOSOPHERS; i++) {
+        for(int i = 0; i < philosophers; i++) {
             threads[i].interrupt();
             threads[i].join();
         }
